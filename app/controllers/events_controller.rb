974f1a132a8d4299707a4404a6a8be008e657730
Daichi Onda <daichi.onda@gmail.com>
2016-07-17 18:33:58 -0700

    added next day/prev day buttons, fixed all bugs, if you return to month, goes to month you were checking out

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,9 +4,10 @@ class EventsController < ApplicationController
 	end
 
 	def index
+        # logic to pick the events based on what day or if all events was selected
 		@events = Event.all
         if params[:show_all]
-            return @events
+            @events
         else
             @result = []
             @events.each do |event|
@@ -15,6 +16,27 @@ class EventsController < ApplicationController
                 end
             end
             @events = @result
+        end
+
+        # passing params to create to be able to return to correct one
+        if params[:show_all]
+            @state = 'true'
+        elsif params[:date] != nil
+            @state = params[:date]
+        else
+            @state = ''
+        end
+
+        # variable to allow next day and previous day to be done and return to current month
+        if params[:date] != nil
+            @day = params[:date][3,2]
+            @month = params[:date][0,2]
+            @year = params[:date][6,2]
+            @year = (2000 + @year.to_i).to_s
+            @date = @year + "/" + @month + "/" + @day
+            @date = @date.to_date
+            @nextDay = (@date + 1).strftime('%m/%d/%y')
+            @prevDay = (@date - 1).strftime('%m/%d/%y')
         end
 	end
 
